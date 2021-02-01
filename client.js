abcc367338acf05617b0b87fc95576ba10949436
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-01 16:36:34 -0600

    git commit

--- client.js
+++ client.js
@@ -1,14 +1,26 @@
-console.log('Here are all the available people:', people);
-
 $(document).ready(andGo);
 
 function andGo() {
-  console.log('jquery loaded');
+  const selector = randomNumber(0, people.length - 1);
+
+  // prompt for selection
+  $('.place-header-here').append(`
+    <h1>
+      Please select ${people[selector].name}
+    </h1>
+
+  `);
+
+  // display all peeps
   people.forEach((obj) => {
-    $('.place-here').append(`
+    $('.place-image-here').append(`
     <div class="image-display">
     <img src="https://github.com/${obj.githubUsername}.png?size=250" alt="Profile image of ${obj.name}">
     </div>
     `);
   });
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (1 + max - min) + min);
 }
